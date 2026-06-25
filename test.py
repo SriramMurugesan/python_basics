47bcbe10757f2f1e4297cd8bad9c8f2dd404281b
SriramMurugesan <sri2014ram@gmail.com>
2026-06-25 16:51:28 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,29 @@
+# print("hello world","world",sep="#")
+# print("hello world",end="")
+# print("hello world")
+# print("10.5"*2)
+# y="s"
+# print("abc">"abd")
+# x=int(input())
+# print(int(float(x)))
+# print("\test\ntesting")
+# print(r"\test\ntesting")
+# print("\\test\\ntesting")
+# print("""helllo
+#       my name is
+#       John
+#       """)
+# x="hello"
+# print(x[0:3:2]) # start:end:step
+# print(x[::-1]) # reverse string
+# l=[1,2,3,4,5]
+# print(l[::2]) # start:end:step
+for i in range(0,10,2):
+        print(i)
+# while True:
+#     print("hello")
+
+    
+
+
+
